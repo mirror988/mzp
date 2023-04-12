f4cab77504dfd704f606873ee11f3a16c2e1840c
mzp <1666907336@qq.com>
2023-04-12 14:51:46 +0800

    文件上传

---
+++ src/main/java/org/gulingjingguai/mzp/MzpApplication.java
@@ -0,0 +1,13 @@
+package org.gulingjingguai.mzp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MzpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MzpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/gulingjingguai/mzp/MzpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.gulingjingguai.mzp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MzpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
