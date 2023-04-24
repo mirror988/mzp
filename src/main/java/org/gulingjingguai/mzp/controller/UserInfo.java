183c557324062009f204643dfa4c6504e3bf2899
mzp <1666907336@qq.com>
2023-04-24 14:51:31 +0800

    方法名修改

--- src/main/java/org/gulingjingguai/mzp/controller/UserInfo.java
+++ src/main/java/org/gulingjingguai/mzp/controller/UserInfo.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfo {
     @GetMapping("/basicInfo")
     public String getUserInfo(){
-        return "MiaoZepu";
+        return null;
     }
 }
