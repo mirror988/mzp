599427aafe0ce3593c775af6cd766911af8b0647
mzp <1666907336@qq.com>
2023-06-03 13:43:23 +0800

    1.新加了swagger 2.单元测试了getUserInfo方法 3.pom.xml依赖分块从start->end

--- src/test/java/org/gulingjingguai/mzp/controller/UserInfoControllerTest.java
+++ src/test/java/org/gulingjingguai/mzp/controller/UserInfoControllerTest.java
@@ -1,8 +1,24 @@
 package org.gulingjingguai.mzp.controller;
 
+import org.gulingjingguai.mzp.entity.UserInfoBo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 
 @SpringBootTest
 class UserInfoControllerTest {
+
+    @Autowired
+    private UserInfoController userInfoController;
+
+    @Test
+    void testGetUserInfo() {
+        UserInfoBo userInfoBo = userInfoController.getUserInfo();
+        assertNull(userInfoBo);
+    }
 
 }
