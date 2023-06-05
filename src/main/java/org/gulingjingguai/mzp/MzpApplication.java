456e0dfe7b25a3f0e1cfcf2df8c13ff5b1f5ae92
mzp <1666907336@qq.com>
2023-06-05 09:23:30 +0800

    指定加载类

--- src/main/java/org/gulingjingguai/mzp/MzpApplication.java
+++ src/main/java/org/gulingjingguai/mzp/MzpApplication.java
@@ -2,8 +2,12 @@ package org.gulingjingguai.mzp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import sun.misc.Contended;
 
 @SpringBootApplication
+@Contended
+@EnableScheduling
 public class MzpApplication {
     public static void main(String[] args) {
         SpringApplication.run(MzpApplication.class, args);
