cbb5656da4d68a2bd593d6c1d5b502eeb3e486ce
mirror988 <1666907336@qq.com>
2023-06-25 22:05:24 +0800

    JVM运行时区

--- src/main/java/org/gulingjingguai/mzp/MzpApplication.java
+++ src/main/java/org/gulingjingguai/mzp/MzpApplication.java
@@ -5,11 +5,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import sun.misc.Contended;
 
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
 @SpringBootApplication
 @Contended
 @EnableScheduling
 public class MzpApplication {
     public static void main(String[] args) {
         SpringApplication.run(MzpApplication.class, args);
+    }
+
+    @PostConstruct
+    void setDefaultTimeZone(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
     }
 }
