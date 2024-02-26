7b1ff9f48464c7752fe8752f0b4a49d727b77a18
mirror988 <1666907336@qq.com>
2024-02-26 23:59:42 +0800

    JVM运行时区

--- src/main/java/org/gulingjingguai/mzp/MzpApplication.java
+++ src/main/java/org/gulingjingguai/mzp/MzpApplication.java
@@ -2,7 +2,6 @@ package org.gulingjingguai.mzp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import sun.misc.Contended;
 
